25ad85daf2467ff24d92ceee246175691b9eb75f
gintoki05 <ajieprastyo@gmail.com>
2018-09-21 07:31:05 +0700

    Add UPDATE /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // LIBRARY
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
@@ -78,6 +79,35 @@ app.delete('/todos/:id', (req, res) => {
     })
     .catch(e => {
       res.status(404).send();
+    });
+});
+
+// UPDATE
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
     });
 });
 
